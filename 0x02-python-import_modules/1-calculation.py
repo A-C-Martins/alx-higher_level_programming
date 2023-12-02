be800633971a09a4863e0542a6399e370370bef6
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-12-02 07:54:34 -0800

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """print the sum of a and b"""
 
-    from calculator_1 import add, mul, sub, div
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
