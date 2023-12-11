63a241d3d0a097fa66266c00bba7201d2caca077
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-12-10 19:16:55 -0800

    Replace c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    #new_string = my_string.replace("c", "")
-    for str in my_string:
-        if str == "c" or "C":
-            new_string = my_string.replace("c", "")
-            modi = new_string.replace("C", "")
-    return modi
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
