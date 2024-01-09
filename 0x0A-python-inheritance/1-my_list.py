05977fd2fdffa5c2085285bb0010b819ea587a16
A-C-Martins <arohchukwuebuka91@gmail.com>
2024-01-08 19:23:01 -0800

    correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-The container of MyList class inherit from list
-"""
+'''
+class MyList that inherits from list
+'''
 
 
 class MyList(list):
-    """MyList class that inherits from list"""
-    def __init__(self):
-        """To initialize the object"""
-        super().__init__()
-
+    '''
+    prints the list, but sorted (ascending sort)
+    '''    
+    
     def print_sorted(self):
-        """prints the sorted list"""
-        print(sorted(self))
+        print(sorted(self))   
